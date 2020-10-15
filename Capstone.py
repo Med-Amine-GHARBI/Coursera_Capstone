4dfea5e36fb5e6abcd888c22a1a359177fb2c6ba
Med-Amine-GHARBI <72947818+Med-Amine-GHARBI@users.noreply.github.com>
2020-10-15 23:24:43 +0100

    Add files via upload

---
+++ Capstone.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Coursera_Capstone
+
+# This Notebook will be mainly used for the IBM capstone's project
+
+# In[1]:
+
+
+import pandas as pd
+import numpy as np
+
+
+# In[2]:
+
+
+print ('Hello Capstone Project Course!')
+
+
+# In[ ]:
+
+
+
+
